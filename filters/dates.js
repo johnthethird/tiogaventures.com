5f52a34dcdddd6b1f97b57559247a0a92d7e4b76
John Lynch <johnrlynch@gmail.com>
2020-02-21 13:30:57 -0800

    x

--- filters/dates.js
+++ filters/dates.js
@@ -2,32 +2,37 @@
 A date formatter filter for Nunjucks
 */
 module.exports = function(date, part) {
-  var d = new Date(date);
-  if(part == 'year') {
-    return d.getUTCFullYear();
-  }
-  var month = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December"
-  ];
-  var ordinal = {
-    1 : "st",
-    2 : "nd",
-    3 : "rd",
-    21 : "st",
-    22 : "nd",
-    23 : "rd",
-    31 : "st"
-  };
-  return d.getDate() + (ordinal[d.getDate()] || "th") + " " + month[d.getMonth()] + ", " + d.getUTCFullYear();
+  var d = new Date(date)
+  return d.getMonth() + 1 + '/' + d.getDate() + '/' + d.getUTCFullYear()
 }
+
+// module.exports = function(date, part) {
+//   var d = new Date(date);
+//   if(part == 'year') {
+//     return d.getUTCFullYear();
+//   }
+//   var month = [
+//     "January",
+//     "February",
+//     "March",
+//     "April",
+//     "May",
+//     "June",
+//     "July",
+//     "August",
+//     "September",
+//     "October",
+//     "November",
+//     "December"
+//   ];
+//   var ordinal = {
+//     1 : "st",
+//     2 : "nd",
+//     3 : "rd",
+//     21 : "st",
+//     22 : "nd",
+//     23 : "rd",
+//     31 : "st"
+//   };
+//   return d.getDate() + (ordinal[d.getDate()] || "th") + " " + month[d.getMonth()] + ", " + d.getUTCFullYear();
+// }
