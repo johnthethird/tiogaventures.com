10ee6f32ae3bc4544bc79aa61d6217265cbfabcc
John Lynch <johnrlynch@gmail.com>
2020-02-21 16:34:25 -0800

    x

--- filters/timestamp.js
+++ filters/timestamp.js
@@ -1,7 +1,8 @@
 /*
   A simple ISO timestamp for Nunjucks
 */
-module.exports = function (date) {
-  let timestamp = new Date()
-  return timestamp.getFullYear() + "-" + (timestamp.getMonth() + 1) + "-" + timestamp.getDate()
+module.exports = function(date) {
+  let timestamp = new Date(date)
+  return timestamp.toISOString().slice(0, 10)
+  //return timestamp.getFullYear() + "-" + (timestamp.getMonth() + 1) + "-" + timestamp.getDate()
 }
